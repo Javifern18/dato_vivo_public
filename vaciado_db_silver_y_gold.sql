d7356174cacb9c5c9ab83dc9e21f172bbd9f0c4d
Javifern18 <117264816+Javifern18@users.noreply.github.com>
2022-12-13 12:20:49 +0100

    Create vaciado_db_silver_y_gold.sql

---
+++ vaciado_db_silver_y_gold.sql
@@ -0,0 +1,21 @@
+use dev_silver_db_alumno18;
+drop schema dbt_jfernandezmorilla;
+drop schema google_sheets;
+drop schema sql_server_dbo;
+drop schema snapshots;
+
+use dev_gold_db_alumno18;
+drop schema dbt_jfernandezmorilla;
+drop schema core;
+
+-----------------------------------------------------------
+
+use pro_silver_db_alumno18;
+drop schema dbt_jfernandezmorilla;
+drop schema google_sheets;
+drop schema sql_server_dbo;
+drop schema snapshots;
+
+use pro_gold_db_alumno18;
+drop schema dbt_jfernandezmorilla;
+drop schema core;
